7175317cf69dd6734441904462eef04de6232cc2
Shrijit007 <shrijitraj2023@gmail.com>
2024-09-23 01:39:36 +0530

    updated

---
+++ src/Semm_III/Lab2/Q5.java
@@ -0,0 +1,18 @@
+package Semm_III.Lab2;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Nth Fibonacci number: "+nthfibbonacci(n));
+    }
+
+    public static int nthfibbonacci(int n){
+        if(n <= 1){
+            return n;
+        }
+        return nthfibbonacci(n-1) + nthfibbonacci(n-2);
+    }
+}
